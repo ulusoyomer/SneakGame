670a33409fe17f75eac8ed363aa79b15f2ad1600
Ömer ulusoy <ulusoy.omer09@gmail.com>
2019-08-28 19:58:04 +0300

    Tasarım Tamamlandı

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,12 +31,16 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.labelExit = new System.Windows.Forms.Label();
+            this.labelPuan = new System.Windows.Forms.Label();
+            this.labelScore = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(31)))), ((int)(((byte)(63)))));
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(31)))), ((int)(((byte)(38)))));
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
@@ -45,6 +49,11 @@
             // 
             // panel2
             // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(133)))), ((int)(((byte)(27)))));
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.labelScore);
+            this.panel2.Controls.Add(this.labelPuan);
             this.panel2.Controls.Add(this.labelExit);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
             this.panel2.Location = new System.Drawing.Point(500, 0);
@@ -56,13 +65,62 @@
             // 
             this.labelExit.AutoSize = true;
             this.labelExit.Font = new System.Drawing.Font("Verdana", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.labelExit.Location = new System.Drawing.Point(165, 4);
+            this.labelExit.ForeColor = System.Drawing.Color.DarkRed;
+            this.labelExit.Location = new System.Drawing.Point(164, 0);
             this.labelExit.Name = "labelExit";
             this.labelExit.Size = new System.Drawing.Size(36, 32);
             this.labelExit.TabIndex = 0;
             this.labelExit.Text = "X";
             this.labelExit.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             this.labelExit.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // labelPuan
+            // 
+            this.labelPuan.AutoSize = true;
+            this.labelPuan.Font = new System.Drawing.Font("Rockwell", 45F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPuan.ForeColor = System.Drawing.Color.GhostWhite;
+            this.labelPuan.Location = new System.Drawing.Point(16, 48);
+            this.labelPuan.Name = "labelPuan";
+            this.labelPuan.Size = new System.Drawing.Size(163, 68);
+            this.labelPuan.TabIndex = 1;
+            this.labelPuan.Text = "Puan";
+            // 
+            // labelScore
+            // 
+            this.labelScore.AutoSize = true;
+            this.labelScore.Font = new System.Drawing.Font("Rockwell", 45F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelScore.ForeColor = System.Drawing.Color.GhostWhite;
+            this.labelScore.Location = new System.Drawing.Point(61, 116);
+            this.labelScore.Name = "labelScore";
+            this.labelScore.Size = new System.Drawing.Size(62, 68);
+            this.labelScore.TabIndex = 2;
+            this.labelScore.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(31)))), ((int)(((byte)(63)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Rockwell", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.GhostWhite;
+            this.button1.Location = new System.Drawing.Point(6, 245);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(182, 80);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Yeniden Oyna";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(31)))), ((int)(((byte)(63)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Rockwell", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.GhostWhite;
+            this.button2.Location = new System.Drawing.Point(6, 368);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(182, 80);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Durdur";
+            this.button2.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
@@ -88,6 +146,10 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label labelExit;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelScore;
+        private System.Windows.Forms.Label labelPuan;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,14 +18,94 @@ namespace YilanOyunu
             InitializeComponent();
         }
 
+         
+        List<Box> boxesList = new List<Box>();
         private void Form1_Load(object sender, EventArgs e)
         {
+            
+            int size = 18;
+            int x = 0;
+            int y = 0;
+            int margin = 2;
+            for (int i = 0; i < 750; i++)
+            {
+                Box box = new Box(new Point(x,y),new Size(size,size),i,panel1);
+                x += size + margin ;
+                boxesList.Add(box);
+                if ((i+1) % 25 == 0)
+                {
+                    y += size + margin;
+                    x = 0;
+                }
+                
+            }
+            Walls();
+        }
 
+        class Box
+        {
+            public Point Location { get; set; }
+            public Color Color { get; set; }
+            public Size Size { get; set; }
+            public int indis { get; set; }
+            public Panel Panel { get; set; }
+            public PictureBox PictureBox { get; set; }
+            public bool Wall { get; set; }
+            public bool feed { get; set; }
+            public bool queue { get; set; }
+
+            public Box(Point point,Size size,int indis,Panel panel)
+            {
+                Location = point;
+                Size = size;
+                this.indis = indis;
+                Panel = panel;
+                CreatePictureBox();
+                Wall = false;
+                feed = false;
+                queue = false;
+            }
+
+            public void CreatePictureBox()
+            {
+                PictureBox = new PictureBox();
+                PictureBox.Size = Size;
+                PictureBox.Location = Location;
+                PictureBox.BackColor = Color.FromArgb(4,57,108);
+                Panel.Controls.Add(PictureBox);
+            }
+
+            public void setWall()
+            {
+                Wall = true;
+                PictureBox.BackColor = Color.FromArgb(74,78,77);
+            }
         }
 
         private void Label1_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        public void Walls()
+        {
+            for (int i = 0; i <=24; i++)
+            {
+                boxesList[i].setWall();
+            }
+
+            for (int i = 0; i <= 725; i+=25)
+            {
+                boxesList[i].setWall();
+            }
+            for (int i = 725; i <750; i++)
+            {
+                boxesList[i].setWall();
+            }
+            for (int i = 24; i <= 725; i+=25)
+            {
+                boxesList[i].setWall();
+            }
         }
     }
 }
